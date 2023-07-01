1f66dbc0be8d07d190874316eb4d1e456ea35c66
macas99 <marvndude@gmail.com>
2023-07-01 16:11:55 +0200

    WorkoutList section workouts by date/time passed

--- components/Home/WorkoutList.js
+++ components/Home/WorkoutList.js
@@ -1,7 +1,28 @@
 import { useEffect } from 'react';
 import { StyleSheet, View, FlatList, Text } from 'react-native';
 
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const now = new Date();
+  // const now = new Date(2023, 6, 16);
+  const tenDaysAgo = new Date();
+  tenDaysAgo.setDate(now.getDate() - 10);
+
+  if (date.toDateString() === now.toDateString()) {
+    return "Today";
+  } else if (date > tenDaysAgo) {
+    return "Last 10 days";
+  } else if (date.getFullYear() === now.getFullYear()) {
+    return date.toLocaleString('default', { month: 'long' });
+  } else {
+    return date.getFullYear().toString();
+  }
+}
+
 function WorkoutList(props) {
+  // props.workouts must be sorted by date
+  let renderedDates = [];
 
   return (
     <View style={styles.workoutList}>
@@ -11,8 +32,14 @@ function WorkoutList(props) {
         data={props.workouts}
         renderItem={(itemData) => {
           const lastItem = itemData.index === props.workouts.length - 1;
+          const formattedDate = formatDate(itemData.item.date);
+          const isDateRendered = renderedDates.includes(formattedDate);
+          if (!isDateRendered) {
+            renderedDates.push(formattedDate);
+          }
           return (
             <View>
+              {!isDateRendered && <Text style={styles.dateHeader}>{formattedDate}</Text>}
               <Text>{itemData.item.name} - {itemData.item.date}</Text>
             </View>
           );
